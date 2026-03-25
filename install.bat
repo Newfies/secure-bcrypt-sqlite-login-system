e5ed6f32db7caa1106d634d85c7a18b505e409bf
Newfie <157941859+Newfies@users.noreply.github.com>
2026-03-24 19:52:19 -0500

    fixed

--- install.bat
+++ install.bat
@@ -7,9 +7,13 @@ call npm install
 echo Dependencies installed.
 
 IF EXIST ".env" (
-    REM File exists, skip this part
+    echo .env already exists, skipping creation.
 ) ELSE (
-    echo SESSION = %random% > .env
+    echo SESSION=%random% > .env
+    echo PORT=3000 >> .env
+    echo PASSWORD=%random%%random%%random%%random%%random%%random% >> .env
+    echo .env file created successfully.
 )
 
+pause
 exit
